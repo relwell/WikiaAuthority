f0c295b9f2bbf4ff34457418def6366e231b293a
Robert Elwell <robert.elwell@lookout.com>
2015-08-30 14:00:44 -0700

    graceful redis

--- AuthorityReporter/tasks/etl.py
+++ AuthorityReporter/tasks/etl.py
@@ -177,10 +177,13 @@ def edit_distance(wiki_id, api_url, title_object, earlier_revision, later_revisi
     """
     r = redis.StrictRedis(host=app.config['REDIS_HOST'], port=app.config['REDIS_PORT'], db=app.config['REDIS_DB'])
     key = "%s_%s_%s_%s" % (wiki_id, title_object[u'pageid'], str(earlier_revision), str(later_revision))
-    print key
-    result = r.get(key)
-    if result is not None:
-        return float(result)
+    try:
+        result = r.get(key)
+        if result is not None:
+            return float(result)
+    except redis.ConnectionError:
+        # we will just calculate it
+        pass
 
     params = {u'action': u'query',
               u'prop': u'revisions',
