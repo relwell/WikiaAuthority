d98ba5bf8be27edddd177c2a340c3b44c755563d
Robert Elwell <robert.elwell@lookout.com>
2015-08-30 15:25:13 -0700

    expose ingest data to manager

--- AuthorityReporter/manage.py
+++ AuthorityReporter/manage.py
@@ -1,11 +1,12 @@
 from flask.ext.script import Manager
 from app import app
-from AuthorityReporter.commands import analyze_wiki
+from AuthorityReporter.commands import analyze_wiki, ingest_data
 
 manager = Manager(app)
 
 scripts = {
-    'analyze_wiki': analyze_wiki.AnalyzeWiki()
+    'analyze_wiki': analyze_wiki.AnalyzeWiki(),
+    'ingest_data': ingest_data.IngestData()
 }
 
 
