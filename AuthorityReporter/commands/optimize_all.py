1661a7c6e27c47da46a9a61898f9c831d39c0a41
Robert Elwell <robert.elwell@lookout.com>
2015-11-02 18:26:30 -0800

    optimize logging

--- AuthorityReporter/commands/optimize_all.py
+++ AuthorityReporter/commands/optimize_all.py
@@ -11,12 +11,20 @@ class OptimizeAll(Command):
         """
         Drops all indices
         """
+        
         global_coll = solr.global_collection()
-        for doc in solr.get_all_docs_by_query(global_coll, '*:*', fields='id'):
-            solr.collection_for_wiki(doc['id']).optimize()
+        print 'global'
         global_coll.optimize()
+        for doc in solr.get_all_docs_by_query(global_coll, '*:*', fields='id'):
+            print doc['id']
+            solr.collection_for_wiki(doc['id']).optimize()
+        print 'all pages'
         solr.all_pages_collection().optimize()
+        print 'all topics'
         solr.all_topics_collection().optimize()
+        print 'all user pages'
         solr.all_user_pages_collection().optimize()
+        print 'wiki user'
         solr.wiki_user_collection().optimize()
+        print 'user'
         solr.user_collection().optimize()
